5932c035390da0e2300262c237f25e7f6e66f6eb
victormorais123 <moraisinho7@gmail.com>
2021-10-16 16:04:23 -0300

    adicionando classes para correção

---
+++ api-inicial-main/Domain/Commands/GrupoProduto2Command.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace UStart.Domain.Commands
+{
+    
+     public class GrupoProduto2Command
+    {
+        public Guid Id { get;  set; }
+         public string Descricao { get;  set; }
+         public string CodigoExterno { get;  set; }
+
+    }
+}
---
+++ api-inicial-main/Domain/Entities/GrupoProduto.cs
@@ -0,0 +1,28 @@
+
+using System;
+using UStart.Domain.Commands;
+
+namespace UStart.Domain.Entities
+{
+    public class GrupoProduto
+    {
+        public Guid Id { get; private set; }
+         public string Descricao { get; private set; }
+         public string CodigoExterno { get; private set; }
+
+
+         //public GrupoProduto2(){
+         }
+
+
+        // public GrupoProduto2(GrupoProduto2Command command){
+        //     this.Id = command.Id;
+        //     this.Descricao = command.Descricao;
+        //     this.CodigoExterno = command.CodigoExterno;
+        }
+
+    
+
+
+
+
